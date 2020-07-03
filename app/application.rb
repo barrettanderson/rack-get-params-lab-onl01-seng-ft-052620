7efb2715ff846ea6a4e8f7b3d661e4317026b79b
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 15:41:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -17,6 +17,8 @@ class Application
       if @@cart.empty?
         resp.write "Your cart is empty."
       else
+        @@cart.each do |item|
+          resp.write "#{item}"
       end
     else
       resp.write "Path Not Found"
