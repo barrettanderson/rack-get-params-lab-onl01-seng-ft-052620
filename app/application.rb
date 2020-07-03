1ef452ff7f1cc2d25a1972d619e6e313cabb8a8a
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 15:44:44 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,7 +24,8 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["item"]
       if @@items.include?(search_term)
-      
+      end
+
     else
       resp.write "Path Not Found"
     end
