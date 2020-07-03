e24b537a351e95f465fdf286aac6aa05e702e8d1
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 15:45:34 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -26,6 +26,8 @@ class Application
       if @@items.include?(search_term)
         @@cart << search_term
         resp.write "added #{search_term}"
+      else
+        resp.write "We don't have that item"
       end
 
     else
