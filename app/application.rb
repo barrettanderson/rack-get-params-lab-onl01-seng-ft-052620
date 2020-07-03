b1af781f416fd3a530aa3cacc07aaa71a8a32a11
Barrett Anderson <barrettanderson1989@gmail.com>
2020-07-03 15:45:05 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -24,6 +24,8 @@ class Application
     elsif req.path.match(/add/)
       search_term = req.params["item"]
       if @@items.include?(search_term)
+        @@cart << search_term
+        resp.write "adde"
       end
 
     else
